dc3b2f20abddef3f39f0e5a37f382e30c0542cb4
aayush0012 <bhattaayush2006@gmail.com>
2026-06-27 18:00:18 +0530

    learned get and post methods

--- fast.py
+++ fast.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional 
+from pydantic import BaseModel as base
 app = FastAPI()
 @app.get("/")
 def read_root():
@@ -7,3 +8,12 @@ def read_root():
 @app.get("/greet")
 def greet(name:str,age:Optional[int]=12): 
     return {"Message":f"Hello, welcome to FastAPI! {name}", "Age": age}
+class Student(base): 
+    name:str
+    age:int
+    roll_no:int
+@app.post("/create_student")
+def create(student:Student):
+        return{
+            "Message": f"Student name is {student.name},age is {student.age}, and roll number is {student.roll_no}"
+        }
